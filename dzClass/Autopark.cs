82e8ce58540c94db8b516001c2a663face603ed9
Sailats <momakuzmin@gmail.com>
2022-12-03 20:38:55 +0300

    Update

--- dzClass/Autopark.cs
+++ dzClass/Autopark.cs
@@ -2,7 +2,24 @@ public class Autopark
 {
     public string NameOfAutopark{get;set;}
     public List<string> cars = new List<string>();
-    
+    public Autopark (string nameOfAutopark)
+    {
+        NameOfAutopark = nameOfAutopark;
+    }
+    public void AddAuto(string car)
+    {
+        cars.Add(car);
+    }
+
+    public override string ToString()
+    {
+        
+        for (int i = 0; i < cars.Count; i++)
+        {
+            Console.WriteLine(cars[i]);
+        }
+        return $"Autopark {NameOfAutopark}  ";
+    }
 
 
 
--- dzClass/Program.cs
+++ dzClass/Program.cs
@@ -10,11 +10,18 @@ namespace Program
             PassengerCar car2 = new PassengerCar("Audi", 550,2022,5);
             Truck truck1 = new Truck("Mersedes",560,2023,1500,"Ivan");
             
-            Console.WriteLine(truck1.ToString());
-            truck1.ChangeDriver();
-            Console.WriteLine(truck1.ToString());
+            // Console.WriteLine(truck1.ToString());
+            // truck1.ChangeDriver();
+            // Console.WriteLine(truck1.ToString());
+            //car2.GetInfoRepairBook();
+
+
+            Autopark park1 = new Autopark(car1.ToString());
+            park1.AddAuto(car1.ToString());
+            park1.AddAuto(car2.ToString());
+            park1.AddAuto(truck1.ToString());
+            park1.ToString();
 
         }
-
     }
 }
